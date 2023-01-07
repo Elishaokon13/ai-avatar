c6475953108fc9d096107103af38c5280899f5c3
Elisha Okon <eliscodavid@gmail.com>
2023-01-07 11:45:16 +0100

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,10 +1,15 @@
+const bufferToBase64 = (buffer) => {
+  const base64 = buffer.toString('base64');
+  return `data:image/png;base64,${base64}`;
+};
+
 const generateAction = async (req, res) => {
   console.log('Received request');
 
   const input = JSON.parse(req.body).input;
 
   const response = await fetch(
-    `https://api-inference.huggingface.co/models/buildspace/ai-avatar-generator`,
+    `https://api-inference.huggingface.co/models/raza2/abrazaq`,
     {
       headers: {
         Authorization: `Bearer ${process.env.HF_AUTH_KEY}`,
